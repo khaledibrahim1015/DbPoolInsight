c7ae5b6abd2f474661fee70f1809d3bf5ae3534d
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2026-02-27 05:15:30 +0200

    Main setup for EFCore.Observability Library

--- EFCore.Observability.Sample.APIv1/Program.cs
+++ EFCore.Observability.Sample.APIv1/Program.cs
@@ -1,13 +1,49 @@
+using EFCore.Observability.Extensions;
+using EFCore.Observability.Sample.APIv1.Data;
+using EFCore.Observability.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddEFCoreObservability(opts =>
+{
+    opts.TrackRentDurations = true;
+    opts.EnableDiagnosticLogging = true;
+    opts.MaxActivityHistoryPerContext = 500;
+    opts.TrackStandardContexts = true;
+});
+
+
+builder.Services.AddDbContextPool<PrimaryDbContext>(
+   (sp, options) =>
+   {
+       options.UseSqlServer(builder.Configuration.GetConnectionString("TMS_Conn"))
+                .UseObservability<PrimaryDbContext>(sp, poolSize: 128);
+   },
+   poolSize: 128);
+
+// TrackStandardContexts
+builder.Services.AddDbContext<ReplicaDbContext>((sp, options) =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TMSReplica_Conn"));
+});
+
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseEFCoreObservability();
+// or 
+//app.Services.UseEFCoreObservability();
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +57,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+/// <summary>
+/// GET /api/pooldiagnostics/metrics
+/// Get metrics for all tracked contexts
+/// </summary>
+app.MapGet("/diagnostics/efcore/metrics", (DiagnosticsQueryService svc) =>
+    svc.GetAllDetails());
+
+
+
+
 
 app.Run();
